77c95476efa573f9d300800e4cba81bbbae913f2
Unknown <unknown@example.com>
2019-05-14 12:09:00 +0700

    Initial commit

---
+++ Epson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Epson Printing");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Display dimension : xx");
+        }
+    }
+}
---
+++ LaserJet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("laserjet Printing");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Display dimension : yy");
+        }
+    }
+}
